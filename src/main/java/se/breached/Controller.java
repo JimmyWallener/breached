2ae7e97cada401665f2af897dc8c5a4ac793fa1c
Jimmy Wallener <jimmy.wallener@protonmail.com>
2020-10-30 14:36:17 +0100

    1#Rebuilt switch for handling response codes and solved the null issue that froze the application. 2#Made it possible to conduct how many searches you want 3#Enter also works now, and field is emptied on search. 4#Reworked handling of code in Controller 5#Added more comments

--- src/main/java/se/breached/Controller.java
+++ src/main/java/se/breached/Controller.java
@@ -1,7 +1,6 @@
 package se.breached;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import javafx.fxml.FXML;
 
 import javafx.geometry.Insets;
@@ -9,16 +8,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
+import javafx.scene.input.KeyCode;
 import javafx.scene.text.*;
+import se.breached.api.ApiConnection;
 import se.breached.model.Breached;
 import se.breached.validate.ValidateEmail;
 
 public class Controller {
-    // API has {Account} - Email/Passwords and {Parameters} and different {Service}. Need to work out logic for that
-    private final String uri = "https://haveibeenpwned.com/api/v3/breachedaccount/";
-    private final String truncate = "?truncateResponse=false";
-    Image image1 = new Image("/logos/breached.png", true);
+    private final Image compromisedImg = new Image("/logos/breached.png", true);
+    private final Image notCompromisedImg = new Image("/logos/battery.png", true);
     @FXML private ImageView imageView;
     @FXML private TextField emailField;
     @FXML private TextField passwordField;
@@ -29,39 +27,104 @@ public class Controller {
     public void initialize() {
 
         passwordBtn.setOnAction(e -> {
-            /* TODO: Make it possible to clear textFlow for another search. Right now, one search locks textFlow
-               TODO: Handle if value is Null
-               TODO: Create different output results depending if N ≥ 0
-             */
-            HandleResponse hr = new HandleResponse();
-            AtomicInteger number = hr.numberOfBreaches(passwordField.getText());
-            Text breached = new Text("Password has been pwned " + number + " times");
-            breached.setFont(Font.font("Helvetica", FontWeight.BOLD,20));
-            breached.setFill(Color.RED);
-
-            textFlow.getChildren().add(breached);
+            if(textFlow != null){
+                textFlow.getChildren().clear();
+                textFlow.setStyle("-fx-background-color: linear-gradient(from 25% 25% to 100% 100%, #245e7d, #368daa)");
+                imageView.setImage(notCompromisedImg);
+            }
+            hasPasswordBeenCompromised();
+        });
+        passwordField.setOnKeyPressed(e -> {
+            if (e.getCode().equals(KeyCode.ENTER)){
+                if(textFlow != null){
+                    textFlow.getChildren().clear();
+                    textFlow.setStyle("-fx-background-color: linear-gradient(from 25% 25% to 100% 100%, #245e7d, #368daa)");
+                    imageView.setImage(notCompromisedImg);
+                }
+                hasPasswordBeenCompromised();
+            }
         });
 
         emailBtn.setOnAction(e -> {
-            // TODO: Make it possible to clear textFlow for another search. Right now, one search locks textFlow
-            ValidateEmail ve = new ValidateEmail();
-            if(ve.validEmail(emailField.getText())){
+            if(textFlow != null){
+                textFlow.getChildren().clear();
+                textFlow.setStyle("-fx-background-color: linear-gradient(from 25% 25% to 100% 100%, #245e7d, #368daa)");
+                imageView.setImage(notCompromisedImg);
+            }
+            hasEmailBeenCompromised();
+        });
+        emailField.setOnKeyPressed(e -> {
+            if (e.getCode().equals(KeyCode.ENTER)){
+                if(textFlow != null){
+                    textFlow.getChildren().clear();
+                    textFlow.setStyle("-fx-background-color: linear-gradient(from 25% 25% to 100% 100%, #245e7d, #368daa)");
+                    imageView.setImage(notCompromisedImg);
+                }
+                hasEmailBeenCompromised();
+            }
+        });
 
-                String url = String.format("%s%s%s",uri,emailField.getText(),truncate);
-                HandleResponse hr = new HandleResponse();
-                List<Breached> emailData = hr.breachDataFromEmail(url);
+        imageView.setOnMouseClicked(e -> {
+            textFlow.getChildren().clear();
+            textFlow.setStyle("-fx-background-color: linear-gradient(from 25% 25% to 100% 100%, #245e7d, #368daa)");
+            imageView.setImage(notCompromisedImg);
+        });
+
+    }
+    public void hasPasswordBeenCompromised(){
+        HandleResponse hr = new HandleResponse();
+        int number = Integer.parseInt(String.valueOf(hr.numberOfBreaches(passwordField.getText().trim())));
+        passwordField.clear();
+        Text breached;
+        Text description;
+        if(number == 0) {
+            breached = new Text("PASSWORD HAS NOT BEEN BREACHED!\n\n");
+            description = new Text("This password has previously appeared in a data breach and should never be used. If you've ever used it anywhere before, change it!");
+            breached.setFont(Font.font("Helvetica", FontWeight.BOLD,20));
+            textFlow.setStyle("-fx-background-color: green;");
+        } else {
+            breached = new Text("PASSWORD HAS BEEN COMPROMISED " + number + " OF TIMES!\n\n");
+            description = new Text("This password wasn't found in any of the Pwned Passwords loaded into Have I Been Pwned. That doesn't necessarily mean it's a good password, merely that it's not indexed on this site.");
+            breached.setFont(Font.font("Helvetica", FontWeight.BOLD, 20));
+            imageView.setImage(compromisedImg);
+            textFlow.setStyle("-fx-background-color: red;");
+        }
+        textFlow.setPadding(new Insets(100, 100, 0, 100));
+        textFlow.setTextAlignment(TextAlignment.CENTER);
+        textFlow.getChildren().addAll(breached,description);
+
+    }
+    public void hasEmailBeenCompromised(){
+        ValidateEmail ve = new ValidateEmail();
+        if(ve.validEmail(emailField.getText())){
+
+            String uri = "https://haveibeenpwned.com/api/v3/breachedaccount/";
+            String truncate = "?truncateResponse=false";
+            String url = String.format("%s%s%s", uri,emailField.getText().trim(), truncate);
+            HandleResponse hr = new HandleResponse();
+            List<Breached> emailData = hr.breachDataFromEmail(url);
+            emailField.clear();
+
+            if(emailData != null) {
                 emailData.forEach(data -> {
-                    imageView.setImage(image1);
+                    imageView.setImage(compromisedImg);
                     Text text = new Text("Breached website:");
-                    text.setFont(Font.font("Helvetica",FontWeight.BOLD,30));
+                    text.setFont(Font.font("Helvetica", FontWeight.BOLD, 30));
                     textFlow.setLineSpacing(5);
                     textFlow.setPadding(new Insets(30, 80, 30, 80));
                     textFlow.getChildren().add(text);
                     textFlow.getChildren().add(new Text(data.toString()));
                     textFlow.setStyle("-fx-background-color: red;");
                 });
+            } else {
+                String errorMsg = ApiConnection.errorMessage();
+                Text text = new Text(errorMsg);
+                text.setFont(Font.font("Helvetica", FontWeight.BOLD, 15));
+                textFlow.setLineSpacing(5);
+                textFlow.setPadding(new Insets(30, 80, 30, 80));
+                textFlow.getChildren().add(text);
             }
-        });
+        }
     }
 
 }
--- src/main/java/se/breached/api/ApiConnection.java
+++ src/main/java/se/breached/api/ApiConnection.java
@@ -9,9 +9,19 @@ import java.net.URL;
 import se.breached.model.ApiModel;
 
 public class ApiConnection {
+
+    private static String errorMsg;
     private final String apiKey = ApiModel.getApiKey();
 
     public String ApiResponse(String url){
+
+        final String BAD_REQUEST = "Bad request:\nThe account does not comply with an acceptable format (i.e. it's an empty string)";
+        final String UNAUTHORISED = "Unauthorised:\nEither no API key was provided or it wasn't valid";
+        final String FORBIDDEN = "Forbidden:\nNo user agent has been specified in the request";
+        final String NOT_FOUND = "Not found:\nThe account could not be found and has therefore not been pwned";
+        final String EXCEEDED_REQUESTS = "Too many requests:\nThe rate limit has been exceeded";
+        final String NO_SERVICE = "Service unavailable";
+        final String DEFAULT = "Unexpected Error: Undefined Error";
             /*
             * Creates a Https request with headers.
             * statusCode 200 builds a String response
@@ -24,24 +34,40 @@ public class ApiConnection {
             connection.setRequestProperty("User-Agent", "breached-app");
             connection.setRequestProperty("hibp-api-key", apiKey);
             connection.connect();
-            int statusCode = connection.getResponseCode();
-            // TODO: Rebuild switch and handle other response codes with methods.
-            if(statusCode == 200) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                StringBuilder sb = new StringBuilder();
-                String line;
+            int responseCode = connection.getResponseCode();
 
-                while ((line = reader.readLine()) != null) {
-                sb.append(line).append("\n");
-                }
-                reader.close();
-                return sb.toString();
+            switch(responseCode) {
+                case 200:   BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                            StringBuilder sb = new StringBuilder();
+                            String line;
+
+                            while ((line = reader.readLine()) != null) {
+                            sb.append(line).append("\n");
+                            }
+                            reader.close();
+                            return sb.toString();
+                case 400:   errorMsg = BAD_REQUEST;
+                            break;
+                case 401:   errorMsg = UNAUTHORISED;
+                            break;
+                case 403:   errorMsg = FORBIDDEN;
+                            break;
+                case 404:   errorMsg = NOT_FOUND;
+                            break;
+                case 429:   errorMsg = EXCEEDED_REQUESTS;
+                            break;
+                case 503:   errorMsg = NO_SERVICE;
+                            break;
+                default:    errorMsg = DEFAULT;
             }
 
         } catch (IOException e){
             e.printStackTrace();
         }
         return null;
+    }
+    public static String errorMessage() {
+        return ApiConnection.errorMsg;
     }
 
 }
